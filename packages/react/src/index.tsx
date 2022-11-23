9f2241b1dfc6f7183a9d9989afcc348863b0af0b
Gessio Mori <gessiomorin@gmail.com>
2022-11-23 08:43:53 -0300

    initial: configures stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,4 +1,12 @@
 import { colors } from '@gm-ds/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$green700',
+  borderRadius: '$md',
+  height: '$10',
+})
 
 export function App() {
   return <h1 style={{ color: colors.gray800 }}>Hello World</h1>
